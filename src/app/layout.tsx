700e73f60af9d8e6d59dd9de679b0accf3ea242d
pushkarui06 <pushkarui06@gmail.com>
2026-05-09 11:43:43 +0530

    google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Playfair_Display, Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import { LeadPopupProvider } from "@/components/molecules/lead-popup/LeadPopupProvider";
 import { CookieConsent } from "@/components/molecules/cookie-consent";
 import "./globals.css";
+
+const GA_MEASUREMENT_ID = "G-7LXHM29F4R";
 
 const playfairDisplay = Playfair_Display({
   variable: "--font-heading",
@@ -92,6 +95,18 @@ export default function RootLayout({
       <body
         className={`${playfairDisplay.variable} ${inter.variable} antialiased`}
       >
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
         {children}
         <CookieConsent />
         <Toaster position="top-right" />
